37eb9ddcadc8bb720871737f1aa05054c0b1476f
RobWar-code <rowaniar@aol.com>
2023-02-12 12:36:30 +0000

    Add new game, add turn functions and tests

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,29 @@ game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: []
+    choices: ["button1", "button2", "button3", "button4"]
 }
 
-module.exports = {game};
+function newGame () {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
+    showTurn();
+}
+
+function showTurn() {
+
+}
+
+module.exports = {game, newGame, showScore, addTurn, showTurn};
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame, showScore, addTurn, showTurn } = require("../game");
 
 beforeEach(() => {
     let fs = require("fs");
@@ -24,5 +24,30 @@ describe("game object contains correct keys", () => {
     });
     test("choices exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("button choices are present", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeEach(() => {
+        game.score = 42;
+        game.currentGame = ["button3, button4, button1, button2"];
+        game.playerMoves = ["button4", "button2", "button1", "button3"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toBe(0);
+    });
+    test("length of current game array should be one", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+    test("should clear game player moves array", () => {
+        expect(game.playerMoves).toEqual([]);
+    });
+    test("should display score equals 0", () => {
+        expect(document.getElementById("score").innerText).toBe(0);
     });
 });
